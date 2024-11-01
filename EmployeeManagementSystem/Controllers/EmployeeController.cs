2ae84d08324d18c885e6b8a792f67412be14ed8d
Ahmed Mohsen <163747371+amohsen98@users.noreply.github.com>
2024-11-01 13:41:47 +0200

    ViewModel

---
+++ EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeManagementSystem.Models;
+using EmployeeManagementSystem.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();  
+        public IActionResult Details(int id)
+        {
+            string msg = "Hello From Action";
+            int temp = 50;
+            List<string> branches = new List<string>();
+
+            branches.Add("cairo");
+            branches.Add("Alex");
+            branches.Add("Assiut");
+            //Additional info to send to view from action , not found in model .. Employee model 
+            //We are sending 4 things this way , the model and the three view data 
+            ViewData["Msg"] = msg;
+            ViewData["Temp"] = temp;
+            ViewData["Branches"] = branches;
+            Employee empModel =  context.Employee.FirstOrDefault(e=>e.Id == id);
+            return View("Details", empModel);
+        }
+
+        public IActionResult DetailsVM(int id)
+        {
+            Employee empModel = context.Employee.Include(e=>e.Department).FirstOrDefault(e => e.Id == id);
+           
+            List<string> branches = new List<string>();
+
+            branches.Add("cairo");
+            branches.Add("Alex");
+            branches.Add("Assiut");
+            //Mapping from empModel to viewmodel 
+            EmpVM empVM = new EmpVM();
+            empVM.EmpName = empModel.Name;
+            empVM.DeptName = empModel.Department.Name;
+            empVM.Color = "Red";
+            empVM.Msg = "Welcome";
+            empVM.Branches = branches;
+            return View("DetailsVM", empVM);
+
+
+
+
+
+           
+
+
+        }
+
+    }
+}
---
+++ EmployeeManagementSystem/ViewModel/EmpVM.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagementSystem.ViewModel
+{
+    public class EmpVM
+    {
+        public string EmpName { set; get; }
+        public string DeptName { get; set; }
+        public List<string> Branches { get; set; }
+        public int Temp { get; set; }
+        public string Msg { get; set; }
+        public string Color { get; set; }
+    }
+}
